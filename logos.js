f8e7499545b2cb905504b9212fd8ffd5858ff023
bp5wz4v622-rgb <bp5wz4v622@privaterelay.appleid.com>
2026-01-04 17:40:36 -0400

    Add logos array with logo details

---
+++ logos.js
@@ -0,0 +1,38 @@
+// Logos desde Cloudflare R2
+export const logos = [
+  {
+    id: 1,
+    file: 'logo1.png',
+    url: 'https://pub-427abfa5785a4d66ba550c33b5c48cd2.r2.dev/logo1.png',
+    bgColor: 'bg-[#FFD700]',
+    label: 'AMARILLO'
+  },
+  {
+    id: 2,
+    file: 'logo2.png',
+    url: 'https://pub-427abfa5785a4d66ba550c33b5c48cd2.r2.dev/logo2.png',
+    bgColor: 'bg-[#00AEEF]',
+    label: 'AZUL'
+  },
+  {
+    id: 3,
+    file: 'logo3.png',
+    url: 'https://pub-427abfa5785a4d66ba550c33b5c48cd2.r2.dev/logo3.png',
+    bgColor: 'bg-white',
+    label: 'BLANCO'
+  },
+  {
+    id: 4,
+    file: 'logo4.png',
+    url: 'https://pub-427abfa5785a4d66ba550c33b5c48cd2.r2.dev/logo4.png',
+    bgColor: 'bg-[#FF4136]',
+    label: 'ROJO'
+  },
+  {
+    id: 5,
+    file: 'logo5.png',
+    url: 'https://pub-427abfa5785a4d66ba550c33b5c48cd2.r2.dev/logo5.png',
+    bgColor: 'bg-black',
+    label: 'NEGRO'
+  }
+];
